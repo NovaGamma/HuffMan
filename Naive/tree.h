b8df17b01042ab65eddfba74cd9d1f12e1426fce
NovaGamma <gammanova41@gmail.com>
2020-12-01 11:37:03 +0100

    moving the modification for the AVl from the NaiveVersion to the OptimizedVersion

--- Naive/tree.h
+++ Naive/tree.h
@@ -15,24 +15,9 @@ typedef struct NodeDic{
     struct NodeDic* right;
 }NodeDic;
 
-typedef NodeDic* TreeDic;
-
-
-
 Tree createNode(char letter);
 void displayTree(Tree tree);
 void add2TreeList(Tree* tree,Tree node);
 int getWeight(Tree tree);
 int getDepth(Tree tree);
 Tree createHuffmanNode(Tree min, Tree min2);
-
-NodeDic* Create_Node_Dic(char letter, char* code, int codeLgt);
-void add_BST_Dic(NodeDic** tree, char letter, char* code, int codeLgt);
-int Tree_Depth_Dic(NodeDic* tree);
-int Balance_Factor_Dic(NodeDic* tree);
-void Right_Rotation_Dic(NodeDic** tree);
-void Left_Rotation_Dic(NodeDic** tree);
-void Balance_Dic(NodeDic** tree);
-void add_node_AVL_Dic(NodeDic** tree, char letter, char* code, int codeLgt);
-TreeDic* Huffman_Into_Avl_Wrapper(Tree tree);
-void Huffman_Into_Avl(Tree treeHuff, TreeDic* treeAvl, char lastPath, char* path, int pathLgt);
--- Otpimized/headers/tree.h
+++ Otpimized/headers/tree.h
@@ -9,10 +9,28 @@ typedef struct Node{
 }Node;
 typedef Node* Tree;
 
+typedef struct NodeDic{
+    char letter;
+    char* code;
+    struct NodeDic* left;
+    struct NodeDic* right;
+}NodeDic;
+typedef NodeDic* TreeDic;
+
 Tree createNode(char letter);
 int getDepth(Tree tree);
 Tree createHuffmanNode(Tree min, Tree min2);
 Node** insert(Node** array,int length,char letter,int pos);
 void displayHuffman(Tree tree);
 
+NodeDic* Create_Node_Dic(char letter, char* code, int codeLgt);
+void add_BST_Dic(NodeDic** tree, char letter, char* code, int codeLgt);
+int Tree_Depth_Dic(NodeDic* tree);
+int Balance_Factor_Dic(NodeDic* tree);
+void Right_Rotation_Dic(NodeDic** tree);
+void Left_Rotation_Dic(NodeDic** tree);
+void Balance_Dic(NodeDic** tree);
+void add_node_AVL_Dic(NodeDic** tree, char letter, char* code, int codeLgt);
+TreeDic* Huffman_Into_Avl_Wrapper(Tree tree);
+void Huffman_Into_Avl(Tree treeHuff, TreeDic* treeAvl, char lastPath, char* path, int pathLgt);
 #endif
