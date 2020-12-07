853348b8643f95e2da636511259b69b2b0567888
NovaGamma <gammanova41@gmail.com>
2020-12-07 18:17:50 +0100

    removed an unused variable

--- Otpimized/Optimized.c
+++ Otpimized/Optimized.c
@@ -14,7 +14,6 @@ int main(){
   TreeDic* dico =  Huffman_Into_Avl_Wrapper(huffman);
   compressText(path,*dico);
   clock_t end = clock();
-  double time_spent = (double)(end - begin);
   printf("Compression done !\n");
   printf("Time needed = %f",(double)(end - begin) / CLOCKS_PER_SEC);
   return 0;
