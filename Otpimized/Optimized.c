a26963d71b9b506a090ff1316cb8a1eecd0eb2c3
FlyingBush <64908554+FlyingBush@users.noreply.github.com>
2020-12-11 08:07:44 +0100

    welcome message + menu

--- Otpimized/Optimized.c
+++ Otpimized/Optimized.c
@@ -33,6 +33,29 @@ void decompression(){
 }
 
 int main(){
-    decompression();
+    int select = 0;
+
+    printf("*************************************************************************************************\n");
+    printf("   Welcome in the text files compression / decompression tool made by\n");
+    printf("Anne HIRIART, Elvin AUPIAIS-BERTHY, Thomas CASASNOVAS and Guillaume GOMEZ.\n\n");
+    printf("*************************************************************************************************\n");
+
+    do {
+        printf("To compress a text file, make sure it is in the same folder as the program, that\nits name is \"input.txt\" and type 1.\n");
+        printf("If you rather want to decompress a file, make sure make sure it is in the same folder\n as the program, that its name is \"output.txt\" and type 2.\n\n");
+        printf("Your choice : ");
+        scanf(" %d%*c", &select);
+        printf("\n\n");
+        switch (select) {
+            case 1:
+                compression();
+                break;
+            case 2:
+                decompression();
+                break;
+            default:
+                select = 0;
+        }
+    } while(select != 0);
   return 0;
 }
